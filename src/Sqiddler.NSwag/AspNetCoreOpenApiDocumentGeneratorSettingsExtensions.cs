98a5bf538ced0acbfa093f9ba3ac11afb3b3e767
Markus Hartmair <m.hartmair@cubido.at>
2025-09-30 16:42:30 +0200

    feat: add Open API extension methods

---
+++ src/Sqiddler.NSwag/AspNetCoreOpenApiDocumentGeneratorSettingsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using NJsonSchema.Generation;
+using NSwag.Generation.AspNetCore;
+using System.Collections.ObjectModel;
+
+namespace Sqiddler.NSwag;
+
+public static class AspNetCoreOpenApiDocumentGeneratorSettingsExtensions
+{
+    /// <summary>
+    /// Adds the <see cref="SqidsNSwagProcessor"/>
+    /// </summary>
+    public static void AddSqids(this AspNetCoreOpenApiDocumentGeneratorSettings configure)
+    {
+        configure.OperationProcessors.Add(new SqidsNSwagProcessor());
+        ((Collection<ISchemaProcessor>)configure.SchemaSettings.SchemaProcessors).Insert(0, new SqidsNSwagProcessor());
+        configure.DocumentProcessors.Add(new SqidsNSwagProcessor());
+    }
+}
---
+++ src/Sqiddler.OpenApi/OpenApiOptionsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.OpenApi;
+
+namespace Sqiddler.OpenApi;
+
+public static class OpenApiOptionsExtensions
+{
+    /// <summary>
+    /// Registers the <see cref="SqidsOpenApiTransformer"/> transformer on the current <see cref="OpenApiOptions"/> instance.
+    /// </summary>
+    public static void ConfigureSqidProcessor(this OpenApiOptions options)
+    {
+        options.AddOperationTransformer<SqidsOpenApiTransformer>();
+        options.AddSchemaTransformer<SqidsOpenApiTransformer>();
+        options.AddDocumentTransformer<SqidsOpenApiTransformer>();
+    }
+}
